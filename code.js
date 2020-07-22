485e6bb18ea3d9b4a0f2244c033ff16323dcfe3a
Rafael Lopez <lpz.raf@gmail.com>
2020-07-21 23:26:59 -0400

    first commit

---
+++ code.js
@@ -0,0 +1,14 @@
+// Globals
+
+// current path
+console.log(__dirname)
+
+// Info about the environment the program is running.
+console.log(process)
+
+// Info about the current module. Has methods.
+console.log(module)
+
+// like window, its the global object NEVER use this.
+console.log(global)
+
